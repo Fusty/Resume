3b9a0fef4b9224495237af44719127a4a3819bf3
Alex Sterling <sterlingalex@gmail.com>
2015-07-28 23:46:56 -0400

    That didn't work, and for good reason

--- js/main.js
+++ js/main.js
@@ -59,8 +59,8 @@ function drawTemplateSelection(){
   });
 
   content += "<br/><h4>Oh and feel free to modify the underlying data too!  (Reverts on page reload or reset)</h4>"+
-    "<br/><a class=\"btn btn-success\" onclick=\"clickFromField();\">Load JSON</a><a class=\"btn btn-danger bottom-margin-1\" onclick=\"clickResetFromField();\">Reset</a>"+
-    "<textarea id=\"json-field\" class=\"form-control \" rows=\"100\" style=\"margin-top:1em;\"></textarea>"+
+    "<br/><a class=\"btn btn-success\" onclick=\"clickFromField();\">Load JSON</a><a class=\"btn btn-danger bottom-margin\" onclick=\"clickResetFromField();\">Reset</a>"+
+    "<br/><br/><textarea id=\"json-field\" class=\"form-control \" rows=\"100\"></textarea>"+
     "<br/><a class=\"btn btn-success\" onclick=\"clickFromField();\">Load JSON</a><a class=\"btn btn-danger\" onclick=\"clickResetFromField();\">Reset</a>"+
     "<br/>";
 
