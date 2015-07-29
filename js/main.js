35b52b9f9ce532efb34834aff7a6dbcf56e29a97
Alex Sterling <sterlingalex@gmail.com>
2015-07-28 23:14:04 -0400

    Missing Text on JSON Field go button

--- js/main.js
+++ js/main.js
@@ -61,7 +61,7 @@ function drawTemplateSelection(){
   content += "<br/><h4>Oh and feel free to modify the underlying data too!  (Reverts on page reload or reset)</h4>"+
     "<br/><a class=\"btn btn-success\" onclick=\"clickFromField();\"></a><a class=\"btn btn-danger\" onclick=\"clickResetFromField();\">Reset</a>"+
     "<br/><textarea id=\"json-field\" class=\"form-control \" rows=\"100\"></textarea>"+
-    "<br/><a class=\"btn btn-success\" onclick=\"clickFromField();\"></a><a class=\"btn btn-danger\" onclick=\"clickResetFromField();\">Reset</a>"+
+    "<br/><a class=\"btn btn-success\" onclick=\"clickFromField();\">Load JSON</a><a class=\"btn btn-danger\" onclick=\"clickResetFromField();\">Reset</a>"+
     "<br/>";
 
   //Place content on page
