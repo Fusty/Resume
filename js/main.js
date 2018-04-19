2557319cd7ba3ce5d41c907d5f78fee364b604b4
Alex Sterling <alex@ethode.com>
2018-04-19 16:11:34 -0400

    Updated Resume (Don't Read Into This Too Much Folks)

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ function drawTemplateSelection(){
     content += '<span class="label label-success label-resume" onclick="loadTemplate(\''+shortName+'.html\', true)">'+name+'</span>';
   });
 
-  content += "<br/><br/><h4>Oh, and feel free to modify the underlying data too!  (Reverts on page reload or reset)</h4>"+
+  content += "<h4>Oh, and feel free to modify the underlying data too!  (Reverts on page reload or reset)</h4>"+
     "&nbsp;<a class=\"btn btn-success\" onclick=\"clickFromField();\">Load JSON from textarea</a>&nbsp;<a class=\"btn btn-danger bottom-margin\" onclick=\"clickResetFromField();\">Reset</a>"+
     "<br/><br/><textarea id=\"json-field\" class=\"form-control \" rows=\"100\"></textarea>"+
     "<br/>&nbsp;<a class=\"btn btn-success\" onclick=\"clickFromField();\">Load JSON from textarea</a>&nbsp;<a class=\"btn btn-danger\" onclick=\"clickResetFromField();\">Reset</a>"+
